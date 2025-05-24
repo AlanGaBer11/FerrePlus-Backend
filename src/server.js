d07308038587142d75fcb614718f6a4036f7d758
AlanGaber11 <alangaber11@gmail.com>
2025-05-23 20:59:24 -0600

    First Commit

---
+++ src/server.js
@@ -0,0 +1,46 @@
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+
+// INICIALIZAR LA APLICACIÓN
+const app = express();
+// CAPA DE SEGURIDAD
+app.use(helmet());
+
+const PORT = process.env.PORT || 3000;
+
+// LÍMITE DE PETICIONES
+const limiter = rateLimit({
+  windowMs: 60 * 1000, // 1 MINUTO
+  limit: 100, // 100 PETICIONES POR MINUTO,
+  message: "¡Demasiadas peticiones!",
+  standardHeaders: true,
+  handler: (req, res) => {
+    console.log("IP Bloqueada, alcanzo el límite de peticiones");
+    res.status(409).json({ error: "Demasiadas peticiones!" });
+  },
+});
+
+// APLICAR EL LÍMITE DE PETICIONES A TODAS LAS RUTAS
+app.use(limiter);
+
+app.use(cors());
+app.use(bodyParser.json());
+
+// RUTA DE BIENVENIDA
+app.get("/", (req, res) => {
+  res.send("Bienvenido a FerrePlus");
+});
+
+// INICIAR EL SERVIDOR
+app.listen(PORT, () => {
+  console.log(`Escuchando en el puerto http://localhost:${PORT}`);
+});
+
+// RUTAS QUE NO EXISTEN
+app.use((req, res, next) => {
+  res.status(404).send("Ruta no encontrada");
+});
