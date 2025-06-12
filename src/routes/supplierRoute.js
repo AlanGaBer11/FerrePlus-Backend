d7f7d368ff61c34e035d2b5028a5dc0e87aaa2f9
alanGarcia <220258@utags.edu.mx>
2025-06-11 20:23:23 -0600

    Implementación de validaciones de supplier

--- src/routes/supplierRoute.js
+++ src/routes/supplierRoute.js
@@ -1,12 +1,21 @@
 const express = require("express");
 const router = express.Router();
 const supplierController = require("../controllers/supplierController");
+const validation = require("../validators/supplierValidator");
 
 router
   .get("/getSuppliers", supplierController.getAllSuppliers)
   .get("/getSupplier/:id", supplierController.getSupplierById)
-  .post("/createSupplier", supplierController.createSupplier)
-  .patch("/updateSupplier/:id", supplierController.updatedSupplier)
-  .delete("/deleteSupplier/:id", supplierController.updatedSupplier);
+  .post(
+    "/createSupplier",
+    validation.createSupplierValidator,
+    supplierController.createSupplier
+  )
+  .patch(
+    "/updateSupplier/:id",
+    validation.createSupplierValidator,
+    supplierController.updatedSupplier
+  )
+  .delete("/deleteSupplier/:id", supplierController.deleteSupplier);
 
 module.exports = router;
---
+++ src/validators/supplierValidator.js
@@ -0,0 +1,40 @@
+const { check, validationResult } = require("express-validator");
+const createSupplierValidator = [
+  check("name")
+    .notEmpty()
+    .withMessage("El nombre es obligatorio")
+    .isLength({ min: 8 })
+    .withMessage("El nombre debe tener al menos 8 caracteres"),
+
+  check("phone")
+    .notEmpty()
+    .withMessage("El teléfono es obligatorio")
+    .isLength({ min: 10 })
+    .withMessage("El teléfono debe tener al menos 10 caracteres")
+    .matches(/^\d+$/)
+    .withMessage("El teléfono debe contener solo números"),
+  check("address")
+    .notEmpty()
+    .withMessage("La dirección es obligatoria")
+    .isLength({ min: 10 })
+    .withMessage("La dirección debe tener al menos 10 caracteres")
+    .matches(/^[a-zA-Z0-9\s,.#-]+$/)
+    .withMessage(
+      "La dirección debe contener solo letras, números y caracteres especiales como , . # -"
+    ),
+  check("email")
+    .notEmpty()
+    .withMessage("El email es obligatorio")
+    .isEmail()
+    .withMessage("El email debe ser válido"),
+
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ success: false, errors: errors.array() });
+    }
+    next();
+  },
+];
+
+module.exports = { createSupplierValidator };
