d3dd62bf72ad643733f55afd80c0a4f9a5a725aa
alanGarcia <220258@utags.edu.mx>
2025-08-10 19:40:32 -0600

    Fix: Patron Singleton

--- src/shared/config/db.js
+++ src/shared/config/db.js
@@ -1,27 +1,45 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize({
-  database: process.env.DB_NAME,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  dialect: "postgres",
-  logging: false,
-  define: {
-    timestamps: true,
-    underscored: true,
-  },
-});
+class Database {
+  constructor() {
+    if (Database.instance) {
+      return Database.instance;
+    }
 
-// Probar la conexión
-sequelize
-  .authenticate()
-  .then(() =>
-    console.log(
-      "Conexión a PostgreSQL establecida correctamente con Sequelize."
-    )
-  )
-  .catch((error) => console.error("Error al conectar a PostgreSQL:", error));
+    this.sequelize = new Sequelize({
+      database: process.env.DB_NAME,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      dialect: "postgres",
+      logging: false,
+      define: {
+        timestamps: true,
+        underscored: true,
+      },
+    });
 
-module.exports = sequelize;
+    // Probar la conexión
+    this.sequelize
+      .authenticate()
+      .then(() =>
+        console.log(
+          "Conexión a PostgreSQL establecida correctamente con Sequelize."
+        )
+      )
+      .catch((error) =>
+        console.error("Error al conectar a PostgreSQL:", error)
+      );
+
+    Database.instance = this;
+  }
+
+  getConnection() {
+    return this.sequelize;
+  }
+}
+
+// Exportar una única instancia
+const database = new Database();
+module.exports = database.getConnection();
